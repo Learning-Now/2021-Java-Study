987310a17765393896c595be1354309bb825c196
DongLee99 <jhnj741@naver.com>
2021-01-22 12:53:02 +0900

    feat:계산기

---
+++ 4week/bringjava/src/main/java/Calculator.java
@@ -0,0 +1,81 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+public class Calculator {
+
+
+    public BigDecimal add(BigDecimal first, String second) {
+
+        BigDecimal sec = new BigDecimal(second);
+        return first.add(sec);
+    }
+
+    public BigDecimal sub(BigDecimal first, String second) {
+
+        BigDecimal sec = new BigDecimal(second);
+        return first.subtract(sec);
+    }
+
+    public BigDecimal mul(BigDecimal first, String second) {
+
+        BigDecimal sec = new BigDecimal(second);
+        return first.multiply(sec);
+    }
+
+    public BigDecimal div(BigDecimal first, String second) {
+
+        BigDecimal sec = new BigDecimal(second);
+        return first.divide(sec);
+    }
+
+    public String[] input() throws IOException {
+        try {
+            BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+            String str = bf.readLine();
+            String arr[] = str.split(" ");
+            return arr;
+        } catch (Exception e) {
+            throw new IllegalArgumentException("잘못된 입력 입니다.");
+        }
+    }
+    public BigDecimal calculateSplitedString(String[] str) {
+        try {
+            BigDecimal result = new BigDecimal(str[0]);
+            if (str.length % 2 == 0) {
+                throw new IllegalArgumentException("잘못된 입력 입니다.");
+            }
+            for (int i = 0; i < str.length - 2; i += 2) {
+                result = cal(result, str[i + 2], str[i + 1].charAt(0));
+            }
+            return result;
+        } catch (Exception e) {
+            throw new IllegalArgumentException("잘못된 입력 입니다.");
+        }
+
+
+    }
+
+    public BigDecimal cal(BigDecimal first, String second, char operator) {
+        if (operator == '+') {
+            return add(first, second);
+        }
+        if (operator == '-') {
+            return sub(first, second);
+        }
+        if (operator == '/') {
+            return div(first, second);
+        }
+        if (operator == '*') {
+            return mul(first, second);
+        }
+        throw new IllegalArgumentException("잘못된 입력 입니다.");
+    }
+    public static void main(String[] args) throws IOException {
+        Calculator cal = new Calculator();
+        System.out.println(cal.calculateSplitedString(cal.input()));
+    }
+}
