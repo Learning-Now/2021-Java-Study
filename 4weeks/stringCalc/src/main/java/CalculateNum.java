b9ca082d9eaafbff3613fb3833bd12ebaf6db2bf
BLINK-ONCE <mkmkqoa@gmail.com>
2021-01-31 07:05:41 +0900

    feat: Add CalculateNum.java

---
+++ 4weeks/stringCalc/src/main/java/CalculateNum.java
@@ -0,0 +1,20 @@
+public class CalculateNum {
+    public int addNum(int a, int b) {
+        return a + b;
+    }
+
+    public int sumNum(int a, int b) {
+        return a - b;
+    }
+
+    public int mulNum(int a, int b) {
+        return a * b;
+    }
+
+    public int divNum(int a, int b) {
+        if (b == 0) {
+
+        }
+        return (int)(a / b);
+    }
+}
