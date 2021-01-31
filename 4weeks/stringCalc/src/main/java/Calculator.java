ebc32fb9cb1903617824da58dca6e8eac564b258
BLINK-ONCE <mkmkqoa@gmail.com>
2021-01-31 10:14:05 +0900

    feat: Add input object

--- 4weeks/stringCalc/src/main/java/Calculator.java
+++ 4weeks/stringCalc/src/main/java/Calculator.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 
 public class Calculator {
     public static void main(String[] args) throws IOException {
-
+        Input input = new Input();
+        input.inputFormula();
     }
 }
