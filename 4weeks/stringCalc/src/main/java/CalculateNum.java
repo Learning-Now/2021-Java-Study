b450a45e748e47b5e6f192a25930e17d15a3cf67
BLINK-ONCE <mkmkqoa@gmail.com>
2021-01-31 17:00:51 +0900

    fix: Fix issue where method does not work
    메소드에 static 선언을 하지 않아 메소드를 호출한 후 인자를
    변수에 저장하지 못하는 문제 수정.

--- 4weeks/stringCalc/src/main/java/CalculateNum.java
+++ 4weeks/stringCalc/src/main/java/CalculateNum.java
@@ -1,17 +1,17 @@
 public class CalculateNum {
-    public int addNum(int a, int b) {
+    public  static double addNum(double a, double b) {
         return a + b;
     }
 
-    public int sumNum(int a, int b) {
+    public  static double subNum(double a, double b) {
         return a - b;
     }
 
-    public int mulNum(int a, int b) {
+    public  static double mulNum(double a, double b) {
         return a * b;
     }
 
-    public int divNum(int a, int b) {
+    public static double divNum(double a, double b) {
         if (b == 0) {
 
         }
