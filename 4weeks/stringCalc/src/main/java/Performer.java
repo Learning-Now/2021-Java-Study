4097c589f90a0e230cad263641b15d5c1245d522
BLINK-ONCE <mkmkqoa@gmail.com>
2021-01-31 17:05:54 +0900

    feat: Add performCal
    계산을 수행하는 메소드 performCal() 추가. 배열과 결과값을
    저장하기 위한 변수들 static 선언.

--- 4weeks/stringCalc/src/main/java/Performer.java
+++ 4weeks/stringCalc/src/main/java/Performer.java
@@ -1,16 +1,32 @@
 public class Performer {
-    private String[] formula;
+    private static String[] formula;
+    private static double result = 0;
 
-    public Performer() {
-
-    }
+    public Performer() {}
     public Performer(String[] formula) {
         this.formula = formula;
     }
 
-    public void performCal(){
-        for (int operator = 0; operator < formula.length; operator += 2) {
-
+    public static void performCal(){
+        for (int operator = 1; operator < formula.length; operator += 2) {
+            if (formula[operator].equals("+")) {
+                result = CalculateNum.addNum(Double.parseDouble(formula[operator - 1]),
+                        Double.parseDouble(formula[operator + 1]));
+            }
+            if (formula[operator].equals("-")) {
+                result = CalculateNum.subNum(Double.parseDouble(formula[operator - 1]),
+                        Double.parseDouble(formula[operator + 1]));
+            }
+            if (formula[operator].equals("*")) {
+                result = CalculateNum.mulNum(Double.parseDouble(formula[operator - 1]),
+                        Double.parseDouble(formula[operator + 1]));
+            }
+            if (formula[operator].equals("/")) {
+                result = CalculateNum.divNum(Double.parseDouble(formula[operator - 1]),
+                        Double.parseDouble(formula[operator + 1]));
+            }
+            formula[operator + 1] = Double.toString(result);
         }
+        System.out.println("computed value : " + (int)result);
     }
 }
