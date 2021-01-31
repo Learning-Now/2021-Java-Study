f54d3f59a17dd0850692916d125b025e429c2a55
BLINK-ONCE <mkmkqoa@gmail.com>
2021-01-31 11:01:16 +0900

    feat: Add isComputable, call Performer class
    계산가능여부를 판단하는 코드를 추가함. 계산가능할 경우에는
    계산을 진행할 Performer class에 있는 메소드를 호출하여
    배열로 저장된 식을 인자로 전달한다.

--- 4weeks/stringCalc/src/main/java/Input.java
+++ 4weeks/stringCalc/src/main/java/Input.java
@@ -4,14 +4,20 @@ import java.io.InputStreamReader;
 
 public class Input {
     private String[] arrayFormula;
+    private boolean isComputable = true;
 
     public void inputFormula() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         this.arrayFormula = br.readLine().split(" ");
         for (int i = 1; i < arrayFormula.length; i += 2) {
             if (isElementInt(arrayFormula[i - 1]) == false || isElementInt(arrayFormula[i + 1]) == false) {
+                isComputable = false;
                 break;
             }
+        }
+        if (isComputable) {
+            Performer performer = new Performer();
+            performer.setFormula(arrayFormula);
         }
     }
 
