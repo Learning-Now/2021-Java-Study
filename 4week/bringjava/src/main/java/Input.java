5e545beba31e7ed4759fa139835b0f45dc134719
DongLee99 <jhnj741@naver.com>
2021-02-01 16:04:03 +0900

    fix:input 수정

--- 4week/bringjava/src/main/java/Input.java
+++ 4week/bringjava/src/main/java/Input.java
@@ -2,17 +2,17 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class IntInput {
-    public String[] input() throws IOException {
+public class Input {
+    public String[] stringInput() throws IOException {
         try {
             BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
             String str = bf.readLine();
-            return split(str);
+            return stringSplit(str);
         } catch (IllegalArgumentException e) {
             throw new IllegalArgumentException("잘못된 입력 입니다.");
         }
     }
-    private String [] split(String str) throws IOException {
+    private String [] stringSplit(String str) throws IOException {
         try {
             String arr[] = str.split(" ");
             return arr;
--- 4week/bringjava/src/main/java/Main.java
+++ 4week/bringjava/src/main/java/Main.java
@@ -2,10 +2,10 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        IntInput input = new IntInput();
+        Input input = new Input();
         Cal cal1 = new Cal();
         View view = new View();
-        view.print(cal1.calculateStart(input.input()));
+        view.print(cal1.calculateStart(input.stringInput()));
 
     }
 }
