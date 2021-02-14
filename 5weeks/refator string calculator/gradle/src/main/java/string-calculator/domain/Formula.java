9bd63e54d700582a7cbf07237eb227408fd51c9f
BLINK-ONCE <mkmkqoa@gmail.com>
2021-02-14 21:18:51 +0900

    refactor: Remove FormulaRepository

--- 5weeks/refator string calculator/gradle/src/main/java/string-calculator/domain/Formula.java
+++ 5weeks/refator string calculator/gradle/src/main/java/string-calculator/domain/Formula.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 
 public class Formula {
     private static String[] arrayFormula;
-    private boolean isComputable;
+    private boolean isComputable = true;
 
     public void inputFormula() throws IOException {
         BufferedReader bufferedReader = new BufferedReader((new InputStreamReader(System.in)));
