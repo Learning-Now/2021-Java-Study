414f15bf727b4bd9ab7a1ce4e2a8cde76be895c5
BLINK-ONCE <mkmkqoa@gmail.com>
2021-02-14 19:57:11 +0900

    refactor: Fix issue where Exception didnt work
    예외처리 클래스에서 'unhandled exception java.io.ioexception'
    에러가 뜨는 것을 수정.

--- 5weeks/refator string calculator/gradle/src/main/java/string-calculator/domain/Exception.java
+++ 5weeks/refator string calculator/gradle/src/main/java/string-calculator/domain/Exception.java
@@ -1,20 +1,22 @@
+import java.io.IOException;
+import java.lang.IllegalArgumentException;
+
 public class Exception {
+/*
+    private String[] formula;
+    private boolean isComputable = true;
 
-    public boolean checkOperand(String[] formula, boolean isComputable){
-        for (int i = 1; i < formula.length; i += 2){
-            if (isElementInt(formula[i - 1]) == false || isElementInt(formula[i + 1]) == false) {
-                isComputable = false;
-                break;
-            }
-        }
-        return isComputable;
+    public Exception() {
+        this.formula = Formula.getFormula();
+        this.isComputable = true;
     }
-
-    private boolean isElementInt(String str) {
+*/
+    public static boolean isElementInt(String str) {
         try {
             Integer.parseInt(str);
             return true;
         } catch (IllegalArgumentException e) {
+            e.printStackTrace();
             System.out.println("입력된 값이 숫자가 아닙니다.");
             return false;
         }
--- 5weeks/refator string calculator/gradle/src/main/java/string-calculator/domain/Formula.java
+++ 5weeks/refator string calculator/gradle/src/main/java/string-calculator/domain/Formula.java
@@ -3,18 +3,28 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Formula {
-    private String[] arrayFormula;
-    private boolean isComputable = true;
-
+    private static String[] arrayFormula;
+    private boolean isComputable;
 
     public void inputFormula() throws IOException {
         BufferedReader bufferedReader = new BufferedReader((new InputStreamReader(System.in)));
         this.arrayFormula = bufferedReader.readLine().split(" ");
+        checkOperand();
+    }
+
+    public boolean checkOperand() throws IllegalArgumentException {
+        for (int i = 1; i < arrayFormula.length; i += 2) {
+            if (Exception.isElementInt(arrayFormula[i - 1]) == false || Exception.isElementInt(arrayFormula[i + 1]) == false) {
+                isComputable = false;
+                break;
+            }
+        }
+        return isComputable;
+    }
+
+    public static String[] getFormula() {
+        return arrayFormula;
     }
 
     Exception exception = new Exception();
-    isComputable = exception.checkOperand(arrayFormula, isComputable);
-
-
-
 }
