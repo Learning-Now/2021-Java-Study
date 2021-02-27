92db7064db27f268d297cb58c3cffb840103bfe1
jeongyuneo <piq2255@gmail.com>
2021-02-27 13:21:40 +0900

    refactor: 상수명 수정

--- 6주차/프로그래밍 과제/src/main/java/service/Calculator.java
+++ 6주차/프로그래밍 과제/src/main/java/service/Calculator.java
@@ -7,19 +7,19 @@ import view.PrinterOutputView;
 
 public class Calculator {
 
-    private final int FIRSTARGUMENT = 0;
-    private final int NEXTARGUMENT = 1;
-    private final int NEXTOPERRATOR = 2;
+    private final int FIRST_ARGUMENT = 0;
+    private final int NEXT_ARGUMENT = 1;
+    private final int NEXT_OPERRATOR = 2;
     private int result;
 
     public void calculateFormula(String[] formula) {
         FormulaSplitter formulaSplitter = new FormulaSplitter();
 
-        result = formulaSplitter.splitNumber(formula[FIRSTARGUMENT]);
-        for (int i = 1; i < formula.length; i += NEXTOPERRATOR) {
+        result = formulaSplitter.splitNumber(formula[FIRST_ARGUMENT]);
+        for (int i = 1; i < formula.length; i += NEXT_OPERRATOR) {
             Number calculatedResult = new Number(result);
             Operator operator = new Operator(formula[i]);
-            Number nextArgument = new Number(formulaSplitter.splitNumber(formula[i + NEXTARGUMENT]));
+            Number nextArgument = new Number(formulaSplitter.splitNumber(formula[i + NEXT_ARGUMENT]));
             result = Operators.operateFormula(calculatedResult, operator, nextArgument);
         }
         PrinterOutputView.printOutputView(result);
