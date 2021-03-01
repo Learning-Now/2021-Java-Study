3841991323e6572a6fc7c666a6fa96dd4904c9fb
YeonSooYeon <dustndus8@naver.com>
2021-03-01 19:50:48 +0900

    docs: 매직넘버 수정

--- 6주차과제/프로그래밍과제1/java/util/Calculator.java
+++ 6주차과제/프로그래밍과제1/java/util/Calculator.java
@@ -1,49 +1,55 @@
 package util;
+
 import input.InputUtils;
+
 public class Calculator {
+    private static final int FIRST_NUMBER = 0;
+    private static final int OPERAND = 1;
+    private static final int NUMBER = 2;
+
     private int sum;
 
     private Operator op = new Operator();
     private ToInt ti = new ToInt();
     private InputCheck ic = new InputCheck();
 
-    public void checkFirstInput(String[] sps){
-        ic.InputCheck(sps[0]);  // 0번째 값이 숫자인지 확인
-        sum = ti.toInt(sps[0]);
+    public void checkFirstInput(String[] sps) {
+        ic.InputCheck(sps[FIRST_NUMBER]);  // 0번째 값이 숫자인지 확인
+        sum = ti.toInt(sps[FIRST_NUMBER]);
     }
 
-    public void calculatePlus(String[] sps){
-        for (int i = 0; i < sps.length - 2; i += 2) {
-            ic.InputCheck(sps[i + 2]);  // 2,4,6 등 짝수 번째 인덱스가 숫자인지 확인
-            if (sps[i + 1].equals("+")) {   // 1,3,5 등 홀수 번째 인덱스가 연산자와 같을 때
-                sum = op.add(sum, ti.toInt(sps[i + 2]));
+    public void calculatePlus(String[] sps) {
+        for (int i = FIRST_NUMBER; i < sps.length - NUMBER; i += NUMBER) {
+            ic.InputCheck(sps[i + NUMBER]);  // 2,4,6 등 짝수 번째 인덱스가 숫자인지 확인
+            if (sps[i + OPERAND].equals("+")) {   // 1,3,5 등 홀수 번째 인덱스가 연산자와 같을 때
+                sum = op.add(sum, ti.toInt(sps[i + NUMBER]));
             }
         }
     }
 
-    public void calculateSubtraction(String[] sps){
-        for (int i = 0; i < sps.length - 2; i += 2) {
-            ic.InputCheck(sps[i + 2]);  // 2,4,6 등 짝수 번째 인덱스가 숫자인지 확인
-            if (sps[i + 1].equals("-")) {
-                sum = op.sub(sum, ti.toInt(sps[i + 2]));
+    public void calculateSubtraction(String[] sps) {
+        for (int i = FIRST_NUMBER; i < sps.length - NUMBER; i += NUMBER) {
+            ic.InputCheck(sps[i + NUMBER]);  // 2,4,6 등 짝수 번째 인덱스가 숫자인지 확인
+            if (sps[i + OPERAND].equals("-")) {
+                sum = op.sub(sum, ti.toInt(sps[i + NUMBER]));
             }
         }
     }
 
-    public void calculateMultiplication(String[] sps){
-        for (int i = 0; i < sps.length - 2; i += 2) {
-            ic.InputCheck(sps[i + 2]);  // 2,4,6 등 짝수 번째 인덱스가 숫자인지 확인
-            if (sps[i + 1].equals("*")) {
-                sum = op.multi(sum, ti.toInt(sps[i + 2]));
+    public void calculateMultiplication(String[] sps) {
+        for (int i = FIRST_NUMBER; i < sps.length - NUMBER; i += NUMBER) {
+            ic.InputCheck(sps[i + NUMBER]);  // 2,4,6 등 짝수 번째 인덱스가 숫자인지 확인
+            if (sps[i + OPERAND].equals("*")) {
+                sum = op.multi(sum, ti.toInt(sps[i + NUMBER]));
             }
         }
     }
 
-    public void calculateDivision(String[] sps){
-        for (int i = 0; i < sps.length - 2; i += 2) {
-            ic.InputCheck(sps[i + 2]);  // 2,4,6 등 짝수 번째 인덱스가 숫자인지 확인
-            if (sps[i + 1].equals("/")) {
-                sum = op.div(sum, ti.toInt(sps[i + 2]));
+    public void calculateDivision(String[] sps) {
+        for (int i = FIRST_NUMBER; i < sps.length - NUMBER; i += NUMBER) {
+            ic.InputCheck(sps[i + NUMBER]);  // 2,4,6 등 짝수 번째 인덱스가 숫자인지 확인
+            if (sps[i + OPERAND].equals("/")) {
+                sum = op.div(sum, ti.toInt(sps[i + NUMBER]));
             }
         }
     }
@@ -61,7 +67,7 @@ public class Calculator {
         return sum;
     }
 
-    public void run(){
+    public void run() {
         String inputString;
         String[] strResultArray;
 
