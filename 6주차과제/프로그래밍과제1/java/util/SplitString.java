5a257fc6597593bb43dc55450cbe49016d5c0079
YeonSooYeon <dustndus8@naver.com>
2021-03-01 19:25:21 +0900

    docs: 블필요한 변수 strArray 삭제

--- 6주차과제/프로그래밍과제1/java/util/SplitString.java
+++ 6주차과제/프로그래밍과제1/java/util/SplitString.java
@@ -1,11 +1,7 @@
 package util;
 
 public class SplitString {
-
-    private String[] strArray; //  자른 후 String 배열
-
     public String[] splitString(String input) {
-        strArray = input.split("\\s");
-        return strArray;
+        return input.split("\\s");
     }
 }
