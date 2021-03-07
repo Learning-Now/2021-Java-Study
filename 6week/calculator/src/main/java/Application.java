aee03cf72c494e2ca34387d28b785cfcf66d13d7
DongLee99 <jhnj741@naver.com>
2021-03-07 17:16:43 +0900

    docs: java 스트링 클래스 정리

--- 6week/calculator/src/main/java/Application.java
+++ 6week/calculator/src/main/java/Application.java
@@ -4,7 +4,11 @@ import Service.StringInput;
 import java.io.IOException;
 
 public class Application {
+    private static Object assertThat;
+
     public static void main(String[] args) throws IOException {
+
+
         CalculateStart calculateStart =new CalculateStart();
         calculateStart.stringCalculateStart(new StringInput().stringInput());
     }
