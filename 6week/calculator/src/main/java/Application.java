666c2c3cbf1890ab1379d1db3e8e3b286a316d02
DongLee99 <jhnj741@naver.com>
2021-03-08 16:11:33 +0900

    3/07

--- 6week/calculator/src/main/java/Application.java
+++ 6week/calculator/src/main/java/Application.java
@@ -2,14 +2,17 @@ import Service.CalculateStart;
 import Service.StringInput;
 
 import java.io.IOException;
+import java.util.function.BiFunction;
 
 public class Application {
     private static Object assertThat;
 
     public static void main(String[] args) throws IOException {
+        final BiFunction<Integer ,Integer , Integer> bi = (num1, num2) -> num1+num2;
+        int result = bi.apply(1,2);
 
 
-        CalculateStart calculateStart =new CalculateStart();
-        calculateStart.stringCalculateStart(new StringInput().stringInput());
+        //CalculateStart calculateStart =new CalculateStart();
+        //calculateStart.stringCalculateStart(new StringInput().stringInput());
     }
 }
