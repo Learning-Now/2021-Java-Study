a0f5dca3ec468d83e681597acce5d4d1b8f03f90
DongLee99 <jhnj741@naver.com>
2021-03-10 10:10:36 +0900

    fix:상수명 변경

--- 7week/Calculator2/src/main/java/Service/StringCalculator.java
+++ 7week/Calculator2/src/main/java/Service/StringCalculator.java
@@ -11,18 +11,18 @@ import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class StringCalculator {
-    public static final int FIND_FIRST_NUM = 0;
-    public static final int FIND_OPERATOR = 1;
-    public static final int FIND_SECOND_NUM = 2;
+    public static final int FIND_INDEX_ZERO = 0;
+    public static final int INDEX_INTERVAL_ONE = 1;
+    public static final int INDEX_INTERVAL_TWO = 2;
 
     public void start() {
         String[] arr = BufferInput.stringBufferInput();
-        Number first = new Number(arr[FIND_FIRST_NUM]);
+        Number first = new Number(arr[FIND_INDEX_ZERO]);
         Number result = new Number("0");
 
         for (int index = 0; index < arr.length - 2; index += 2) {
-            Number second = new Number(arr[index + FIND_SECOND_NUM]);
-            result = new Number(Operator.find(arr[index + FIND_OPERATOR]).calculate(first,second));
+            Number second = new Number(arr[index + INDEX_INTERVAL_TWO]);
+            result = new Number(Operator.find(arr[index + INDEX_INTERVAL_ONE]).calculate(first,second));
             first = result;
         }
         View.stringCalculatorResultView(result.getValue());
