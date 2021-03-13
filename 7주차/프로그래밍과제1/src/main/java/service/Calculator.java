a77193353de3cf275d807ca4d69404d7b652430d
jeongyuneo <piq2255@gmail.com>
2021-03-13 13:08:32 +0900

    refactor: 생성자 위치 수정

--- 7주차/프로그래밍과제1/src/main/java/service/Calculator.java
+++ 7주차/프로그래밍과제1/src/main/java/service/Calculator.java
@@ -6,12 +6,12 @@ import view.PrinterOutputView;
 
 public class Calculator {
 
-    private Calculator() {
-    }
-
     private static final int FIRST_INDEX = 0;
     private static final int INDEX_INTERVAL_ONE = 1;
     private static final int INDEX_INTERVAL_TWO = 2;
+
+    private Calculator() {
+    }
 
     public static void start(String[] formula) {
         Number resultNumber = new Number(formula[FIRST_INDEX]);
