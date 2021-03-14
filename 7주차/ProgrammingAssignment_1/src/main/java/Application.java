bcc4bde3091bd79efb62e782da3ce20b9d2cb815
jeongyuneo <piq2255@gmail.com>
2021-03-14 18:22:56 +0900

    fix: 잘못된 메소드명 변경

--- 7주차/ProgrammingAssignment_1/src/main/java/Application.java
+++ 7주차/ProgrammingAssignment_1/src/main/java/Application.java
@@ -4,6 +4,6 @@ import view.BufferedReaderInputView;
 public class Application {
 
     public static void main(String[] args) {
-        Calculator.calculateFormula(BufferedReaderInputView.input());
+        Calculator.start(BufferedReaderInputView.input());
     }
 }
