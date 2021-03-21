c1aaa21c78da0d4612cb9376105acf7445e1838c
jeongyuneo <piq2255@gmail.com>
2021-03-21 21:22:45 +0900

    refactor: Calculator 수정

--- 7주차/ProgrammingAssignment_1/src/main/java/service/Calculator.java
+++ 7주차/ProgrammingAssignment_1/src/main/java/service/Calculator.java
@@ -2,26 +2,34 @@ package service;
 
 import domain.Number;
 import domain.Operator;
+import view.BufferedReaderInputView;
 import view.PrinterOutputView;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Calculator {
 
-    private static final int FIRST_INDEX = 0;
-    private static final int INDEX_INTERVAL_ONE = 1;
     private static final int INDEX_INTERVAL_TWO = 2;
 
-    private Calculator() {
-    }
-
-    public static void start(String[] formula) {
-        Number resultNumber = new Number(formula[FIRST_INDEX]);
+    public void start() {
+        String[] formula = BufferedReaderInputView.input();
+        Queue<Number> numbers = splitFormula(formula);
+        Number resultNumber = numbers.poll();
         for (int index = 1; index < formula.length; index += INDEX_INTERVAL_TWO) {
-            Number calculatedResult = resultNumber;
-            Number nextArgument = new Number(formula[index + INDEX_INTERVAL_ONE]);
             String stringResult = Operator.find(formula[index])
-                    .calculate(calculatedResult, nextArgument);
+                    .calculate(resultNumber, numbers.poll());
             resultNumber = new Number(stringResult);
         }
         PrinterOutputView.printOutputView(resultNumber);
+    }
+
+    private Queue<Number> splitFormula(String[] formula) {
+        return IntStream.range(0, formula.length)
+                .filter(count -> count % 2 == 0)
+                .mapToObj(index -> new Number(formula[index]))
+                .collect(Collectors.toCollection(ArrayDeque::new));
     }
 }
