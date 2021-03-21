a32fa5e5954b9e4793c594e07b004e2dc6adf75a
jeongyuneo <piq2255@gmail.com>
2021-03-21 21:23:40 +0900

    refactor: Calculator 인스턴스화

--- 7주차/ProgrammingAssignment_1/src/main/java/Application.java
+++ 7주차/ProgrammingAssignment_1/src/main/java/Application.java
@@ -1,9 +1,9 @@
 import service.Calculator;
-import view.BufferedReaderInputView;
 
 public class Application {
 
     public static void main(String[] args) {
-        Calculator.start(BufferedReaderInputView.input());
+        Calculator calculator = new Calculator();
+        calculator.start();
     }
 }
