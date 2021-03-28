b8395833899af3f4051bf781f5829c6245d43c40
BLINK-ONCE <mkmkqoa@gmail.com>
2021-03-28 20:35:44 +0900

    style: Add input name, cycle

--- racingcar/src/main/java/racingcar/Application.java
+++ racingcar/src/main/java/racingcar/Application.java
@@ -3,8 +3,17 @@ package racingcar;
 import java.util.Scanner;
 
 public class Application {
+
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
+        String carName;
+        int cycle;
+
+        carName = scanner.next();
+        cycle = scanner.nextInt();
+        String[] carNames = carName.split(",");
+
+        
     }
 }
