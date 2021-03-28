ca7217d4c159b001d45e5c298810b08a531aa87f
jeongyuneo <piq2255@gmail.com>
2021-03-28 13:58:06 +0900

    docs: CQRS.md 추가

--- 8주차/ProgrammingAssignment_2/src/main/java/domain/Car.java
+++ 8주차/ProgrammingAssignment_2/src/main/java/domain/Car.java
@@ -6,8 +6,6 @@ public class Car implements Move {
     private static final int START_INCLUSIVE = 0;
     private static final int END_INCLUSIVE = 9;
     private static final int GO_POINT = 4;
-    private final String name;
-    private int position = 0;
 
     public Car(String name) {
         this.name = name;
