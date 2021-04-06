e834702e43bcdb2703d389a25b0d8fe864167ce0
YeonSooYeon <dustndus8@naver.com>
2021-04-06 13:35:50 +0900

    refactor: 불필요한 주석 제거

--- 8주차과제/RacingCar/java/Application.java
+++ 8주차과제/RacingCar/java/Application.java
@@ -2,7 +2,6 @@ import utils.GameUtils;
 
 public class Application {
     public static void main(String[] args) {
-        //final Scanner scanner = new Scanner(System.in);
         GameUtils gameUtils = new GameUtils();
         gameUtils.run();
     }
