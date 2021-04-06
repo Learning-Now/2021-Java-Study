c05f489011805ad14865cad79b009f36aa578531
YeonSooYeon <dustndus8@naver.com>
2021-04-06 13:54:39 +0900

    refactor: 일급컬렉션 추가

--- 8주차과제/RacingCar/java/inpututils/InputView.java
+++ 8주차과제/RacingCar/java/inpututils/InputView.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner SCANNER = new Scanner(System.in);
 
-    public String getCarName() {
+    public static String getCarName() {
         System.out.print("경주 할 자동차 이름 : ");
         return SCANNER.nextLine();
     }
 
-    public int getTryNumber() {
+    public static int getTryNumber() {
         System.out.print("시도할 횟수 : ");
         try {
             return SCANNER.nextInt();
---
+++ 8주차과제/RacingCar/java/racingcar/Cars.java
@@ -0,0 +1,90 @@
+package racingcar;
+
+import outpututils.OutputView;
+import utils.GameUtils;
+import utils.RandomUtils;
+
+import java.util.List;
+import java.util.Vector;
+import java.util.stream.IntStream;
+
+public class Cars {
+    private static List<Car> carList;
+
+    private static final int START_NUMBER = 0;
+    private static final int END_NUMBER = 9;
+
+    private static Vector<String> winner;
+
+    public Cars(List<Car> carList) {
+        this.carList = carList;
+    }
+
+    public static boolean isMove(int randomNumber) {
+        if (randomNumber >= 4) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void playGame() {
+        for (int j = 0; j < carList.size(); j++) {
+
+            OutputView.printCarName(carList.get(j).getName());
+
+            if (isMove(RandomUtils.nextInt(START_NUMBER, END_NUMBER))) {
+                carList.get(j).plusPosition();
+            }
+            for (int k = 0; k < carList.get(j).getPosition(); k++) {
+                OutputView.printMove();
+            }
+            System.out.println();
+        }
+/*
+        carList.stream()
+                .map(Car::getName)
+                .forEach(car -> OutputView.printCarName(car));
+        for (int i = 0; i < carList.size(); i++) {
+            IntStream.range(0, carList.get(i).getPosition())
+                    .forEach(OutputView.printMove();
+        }
+*/
+    }
+
+    public static int getMax() {
+        int max = carList.get(0).getPosition();
+        for (int i = 1; i < carList.size(); i++) {
+            if (max < carList.get(i).getPosition()) {
+                max = carList.get(i).getPosition();
+            }
+        }
+        return max;
+    }
+    public static int makeWinnerCount(int max) {
+        int count = 0;
+        for (int i = 0; i < carList.size(); i++) {
+            if (max == carList.get(i).getPosition()) {
+                count++;
+            }
+        }
+        return count;
+    }
+    public static void setWinnerVector(int max, int count) {
+        winner = new Vector<String>(count);
+        for (int i = 0; i < carList.size(); i++) {
+            if (max == carList.get(i).getPosition()) {
+                winner.add(carList.get(i).getName());
+            }
+        }
+    }
+    public static Vector<String> getWinnerVector() {
+        return winner;
+    }
+
+
+
+
+
+
+
+}
