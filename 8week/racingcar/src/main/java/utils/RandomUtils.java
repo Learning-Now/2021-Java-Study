bf7ab0b89d2dc158daff881b47dcfccb8f4330d9
DongLee99 <jhnj741@naver.com>
2021-04-07 10:33:40 +0900

    refactor:RandomUtils 컨벤션

--- 8week/racingcar/src/main/java/utils/RandomUtils.java
+++ 8week/racingcar/src/main/java/utils/RandomUtils.java
@@ -4,8 +4,10 @@ import java.util.Random;
 
 public class RandomUtils {
     private static final Random RANDOM = new Random();
+
     private RandomUtils() {
     }
+
     public static int randomIntGenerator(final int startNumber, final int lastNumber) {
         if (startNumber > lastNumber) {
             throw new IllegalArgumentException();
@@ -18,4 +20,5 @@ public class RandomUtils {
         }
         return startNumber + RANDOM.nextInt(lastNumber - startNumber + 1);
     }
+
 }
