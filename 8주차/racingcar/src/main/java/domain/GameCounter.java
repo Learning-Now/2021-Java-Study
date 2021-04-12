e2d68bc6d67f58e09bd6dde28243a133851f6a69
jeongyuneo <piq2255@gmail.com>
2021-04-12 12:46:27 +0900

    refactor: GameCounter - validate 책임 수정

--- 8주차/racingcar/src/main/java/domain/GameCounter.java
+++ 8주차/racingcar/src/main/java/domain/GameCounter.java
@@ -1,24 +1,25 @@
 package domain;
 
 public class GameCounter {
+    private static final int ZERO = 0;
+
     private int counter;
 
     public GameCounter(String round) {
-        int number = validate(round);
-        this.counter = number;
+        validate(round);
+        this.counter = Integer.parseInt(round);
     }
 
-    private int validate(String value) {
+    private void validate(String value) {
         int result;
         try {
             result = Integer.parseInt(value);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 시도 횟수는 숫자여야 한다.");
         }
-        if (result <= 0) {
+        if (result <= ZERO) {
             throw new IllegalArgumentException("[ERROR] 시도 횟수는 자연수여야 한다.");
         }
-        return result;
     }
 
     public int nextRound() {
