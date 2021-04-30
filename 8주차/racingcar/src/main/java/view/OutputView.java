da0bf05d876df1263ddb6c514466a2766135bc5e
jeongyuneo <piq2255@gmail.com>
2021-04-30 19:42:54 +0900

    refactor: 코드 컨벤션

--- 8주차/racingcar/src/main/java/view/OutputView.java
+++ 8주차/racingcar/src/main/java/view/OutputView.java
@@ -35,6 +35,7 @@ public class OutputView {
     public static void printGameCounterInput() {
         System.out.println("시도할 횟수는 몇회인가요? ");
     }
+
     public static void printCarInput() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
