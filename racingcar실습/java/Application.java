1ba67c4239e1bac138db2a31521a6ed7a8067188
YeonSooYeon <dustndus8@naver.com>
2021-05-17 17:40:21 +0900

    refactor: run 불필요한 객체 생성 제거

--- racingcar실습/java/Application.java
+++ racingcar실습/java/Application.java
@@ -2,7 +2,6 @@ import utils.GameUtils;
 
 public class Application {
     public static void main(String[] args) {
-        GameUtils gameUtils = new GameUtils();
-        gameUtils.run();
+        GameUtils.run();
     }
 }
